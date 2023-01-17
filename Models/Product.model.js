6551f72e28ed54fdb2207010a61b0eadd7ddeb0a
Rishav Neogi <nrishav31@gmail.com>
2023-01-17 16:42:03 +0530

    fully ready

---
+++ Models/Product.model.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+const ProductSchema=mongoose.Schema({
+    image:String,
+    name:String,
+    category:String,
+    description:String,
+    price:Number,
+    offer_price:Number
+});
+const ProductModel=mongoose.model("Bymeproduct",ProductSchema);
+module.exports=ProductModel;
