44781b3b9b759cd1a284821d31788af671b045f7
Rishav Neogi <nrishav31@gmail.com>
2023-01-20 12:51:42 +0530

    name and offerprice added in cart schema

--- Models/Cart.model.js
+++ Models/Cart.model.js
@@ -3,8 +3,10 @@ const CartSchema=mongoose.Schema({
     image:String,
     title:String,
     description:String,
-    price:String,
+    price:Number,
     category:String,
+    name:String,
+    offer_price:Number,
     userID:String
 });
 const CartModel=mongoose.model("shop669cart",CartSchema);
