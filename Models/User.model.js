479c9625750abe7fa12590c92c6076b795dd48c3
Rishav Neogi <nrishav31@gmail.com>
2023-01-26 21:28:18 +0530

    added address in user model

--- Models/User.model.js
+++ Models/User.model.js
@@ -3,6 +3,7 @@ const UserSchema=mongoose.Schema({
     name:String,
     email:String,
     password:String,
+    address:String,
     administration:Boolean
 });
 const UserModel=mongoose.model("shop669user",UserSchema);
